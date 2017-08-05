93dc8b6293b4f7fe37e82df8148f2a6f5048e564
Jeff Geerling <geerlingguy@mac.com>
2017-08-04 22:24:43 -0500

    Fixes #6: Make sure to install deps via Composer.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -7,15 +7,16 @@ printf "\n"${green}"Building local environment via Docker Compose."${neutral}"\n
 docker-compose up -d
 
 printf "\n"${green}"Installing the Drupal Photo Gallery site from configuration."${neutral}"\n"
-docker exec d8pix-local bash -c "drush site-install minimal -y \
+docker exec d8pix bash -c "\
+      drush site-install minimal -y  \
       --site-name='Drupal Photo Gallery' \
       --account-pass=admin \
       --config-dir=../config/sync \
-      --db-url=mysql://drupal:drupal@localhost/drupal \
       --uri=local.d8pix.com"
 
 printf "\n"${green}"Checking if the site was installed successfully."${neutral}"\n"
 curl --head "http://local.d8pix.com/"
+curl "http://local.d8pix.com/" | grep "Enter your Drupal Photo Gallery username."
 
 printf "\n"${green}"Tearing down local environment via Docker Compose."${neutral}"\n"
 docker-compose down
